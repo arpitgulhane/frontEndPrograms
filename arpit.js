9a62d927e155ba02c75c3f26124bc0fc37e98f01
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-04-28 02:57:52 +0530

    day 42

---
+++ arpit.js
@@ -0,0 +1,9 @@
+// const prompt=require("prompt-sync")({sigint:true});
+// const prompt = require("prompt-sync")();
+// let name = prompt("What is your name?");
+// console.log("Hello, " + name + "!");
+
+const prompt = require("prompt-sync")();
+const number = parseInt(prompt('Enter a positive integer: '));
+
+
